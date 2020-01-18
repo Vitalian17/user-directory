258620ed81baa12284c4c15e26da8d0c8ff9083b
Tony Sarni <sarni.tony@gmail.com>
2020-01-17 20:43:56 -0600

    pushed all files to github, working offline

---
+++ build/precache-manifest.bf25040b61293b1c1352a4376dd18f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+      "revision": "c076b0f2138049c7dd38fe1cf0bc20ca",
+      "url": "https://michaelrobil.github.io/employeereact/index.html"
+    },
+    {
+      "revision": "64d11ce1f775062eba66",
+      "url": "https://michaelrobil.github.io/employeereact/static/css/main.a582e99b.chunk.css"
+    },
+    {
+      "revision": "795960c7eba3396b68db",
+      "url": "https://michaelrobil.github.io/employeereact/static/js/2.c4222c11.chunk.js"
+    },
+    {
+      "revision": "e928fe768baa9832b5bc57eae021f30c",
+      "url": "https://michaelrobil.github.io/employeereact/static/js/2.c4222c11.chunk.js.LICENSE"
+    },
+    {
+      "revision": "64d11ce1f775062eba66",
+      "url": "https://michaelrobil.github.io/employeereact/static/js/main.1c33afa5.chunk.js"
+    },
+    {
+      "revision": "63466808508f869de173",
+      "url": "https://michaelrobil.github.io/employeereact/static/js/runtime-main.f254175d.js"
+    }
+  ]);
